52c162e75303de79714d82c9e99f1f2bc5a696f8
NeelAvaiya <neelavaiya201@gmail.com>
2026-04-26 18:03:36 +0530

    now its statefull connection new joinee also see which checkbox already checked

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import path from 'node:path';
 import express from 'express';
 import { Server } from 'socket.io'
 
+const CHECKBOX_SIZE = 100;
+const state = {
+    checkboxes: new Array(CHECKBOX_SIZE).fill(false)
+}
+
 async function main() {
     const PORT = process.env.PORT ?? 8000;
 
@@ -19,13 +24,18 @@ async function main() {
 
         socket.on('client:checkbox:change', (data) =>{
             console.log(`[Socket:${socket.id}]:client:checkbox:change`, data);
-            io.emit('server:checkbox:change', data)
+            io.emit('server:checkbox:change', data);
+            state.checkboxes[data.index] = data.checked;
         })
     })
 
     //Express
     app.use(express.static(path.resolve('./public')));
-    app.get('/health', (req, res) => res.json({healthy: true}))
+    app.get('/health', (req, res) => res.json({healthy: true}));
+
+    app.get('/checkboxes', (req, res) =>{
+       return res.json({checkboxes: state.checkboxes})
+    }) 
 
     server.listen(PORT, () => {
         console.log(`server is running on ${PORT}`)
